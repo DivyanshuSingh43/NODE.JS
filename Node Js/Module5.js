e1d68734ce30b7d500f5a2366617446117628193
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-02-14 22:32:15 +0530

    Update Module5.js

--- Node Js/Module5.js
+++ Node Js/Module5.js
@@ -5,4 +5,4 @@ const server = http.createServer(requestHandler);
 const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server is running on address http://localhost:${PORT}`);
-});
+}); 
